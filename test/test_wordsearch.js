472703c262eee568c0923f68f30b76f96d231388
VinceEmond <vince.emond@icloud.com>
2022-02-16 23:23:42 +0000

    Initial Commit

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -1,7 +1,9 @@
+// test_wordsearch.js
+
 const chai = require('chai');
 const assert = chai.assert;
 
-const wordSearch = require('../wordsearch.js')
+const wordSearch = require('../wordsearch.js');
 
 describe("#wordSearch()", function() {
   it("should return false if the word is not present", function() {
@@ -15,7 +17,7 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'FRANK')
+    ], 'FRANK');
 
     assert.isFalse(result);
   });
@@ -31,8 +33,15 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'SEINFELD')
+    ], 'SEINFELD');
 
     assert.isTrue(result);
   });
+
+  it("should return false if the suplied array is empty", function() {
+    const result = wordSearch([], 'FRANK');
+
+    assert.isFalse(result);
+  });
+  
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,35 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+// wordsearch.js
 
-module.exports = wordSearch
+const wordSearch = (lettersGrid, searchWord) => {
+  if (lettersGrid.length === 0) {
+    return false;
+  }
+    
+  const wordsFromRowAr = lettersGrid.map(letters => letters.join(''));
+ 
+  // Searching horizontal rows for word
+  for (const word of wordsFromRowAr) {
+    if (word.includes(searchWord)) {
+      return true;
+    }
+  }
+ 
+  for (let x = 0; x < lettersGrid.length; x++) {
+    let verticalWordAr = [];
+    let verticalWordStr = "";
+    for (let y = 0; y < lettersGrid[0].length; y++) {
+      verticalWordAr.push(lettersGrid[y][x]);
+      verticalWordStr = verticalWordAr.join("");
+    }
+ 
+    if (searchWord === verticalWordStr) {
+      return true;
+    }
+  }
+ 
+  return false;
+};
+  
+module.exports = wordSearch;
+ 
+ 
